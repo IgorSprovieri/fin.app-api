04b7b39921c65e0e0835a8ac3a4cc29425dd2a0d
Igor Sprovieri <igsp1@hotmail.com>
2023-06-13 10:49:51 -0300

    Feat: Validation with yup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,21 @@
 import { Users } from "../models";
 import bcrypt from "bcrypt";
+import { object, string, number, date, InferType } from "yup";
 
 export class UserController {
   async post(req, res) {
+    try {
+      const schema = object({
+        name: string().required(),
+        email: string().email().required(),
+        password: string().required(),
+      });
+
+      await schema.validate(req.body);
+    } catch (error) {
+      return res.status(400).json({ error: error?.message });
+    }
+
     try {
       const found = await Users.findOne({ where: { email: req.body.email } });
       if (found) {
