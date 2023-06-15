c7e2431633687c03c60ce79fa97bf5154885c555
Igor Sprovieri <igsp1@hotmail.com>
2023-06-15 08:56:27 -0300

    Refactor: Login Controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,6 +6,9 @@ import awsS3 from "../libs/awsS3";
 
 export class UserController {
   async post(req, res) {
+    const { body } = req;
+    const { email, password } = body;
+
     try {
       const schema = object({
         name: string().required(),
@@ -13,53 +16,54 @@ export class UserController {
         password: string().required(),
       });
 
-      await schema.validate(req.body);
+      await schema.validate(body);
     } catch (error) {
       return res.status(400).json({ error: error?.message });
     }
 
     try {
-      const userFound = await Users.findOne({
-        where: { email: req.body.email },
-      });
+      const userFound = await Users.findOne({ where: { email: email } });
       if (userFound) {
-        return res.status(400).json({ error: "User already exists" });
+        return res.status(400).json({ error: "User Already Exists" });
       }
 
-      req.body.password = bcrypt.hashSync(req.body.password, 10);
+      body.password = bcrypt.hashSync(password, 10);
 
-      const result = await Users.create({ ...req.body });
-      if (!result) {
-        return res.status(400).json({ error: "User not created" });
+      const createdUser = await Users.create({ ...body });
+      if (!createdUser) {
+        return res.status(400).json({ error: "User Not Created" });
       }
 
-      const payload = { id: result.id };
+      const payload = { id: createdUser.id };
       const secret = process.env.JWT_SECRET || "fin.app";
       const options = { expiresIn: "7d" };
 
-      result.dataValues.token = jwt.sign(payload, secret, options);
-      result.dataValues.password = "";
+      createdUser.dataValues.token = jwt.sign(payload, secret, options);
+      createdUser.dataValues.password = "";
 
-      return res.status(201).json(result.dataValues);
+      return res.status(201).json(createdUser.dataValues);
     } catch (error) {
       return res.status(500).json(error);
     }
   }
 
   async avatar(req, res) {
+    const { body, userId } = req;
+    const { mime, base64 } = body;
+
     try {
       const schema = object({
-        base64: string().required(),
         mime: string().required(),
+        base64: string().required(),
       });
 
-      await schema.validate(req.body);
+      await schema.validate(body);
     } catch (error) {
       return res.status(400).json({ error: error?.message });
     }
 
     try {
-      const userFound = await Users.findByPk(req.userId);
+      const userFound = await Users.findByPk(userId);
       if (!userFound) {
         return res.status(404).json({ error: "User Not Found" });
       }
@@ -75,53 +79,50 @@ export class UserController {
       }
 
       const key = `user_${userFound.id}_${new Date().getTime()}`;
-      const uploadResult = await awsS3.upload(
-        key,
-        req.body.base64,
-        req.body.mime
-      );
+      const uploadResult = await awsS3.upload(key, base64, mime);
       if (uploadResult?.error) {
-        return res.status(400).json({ error: "Image not uploaded" });
+        return res.status(400).json({ error: "Image Not Uploaded" });
       }
 
-      userFound.avatar_url = uploadResult?.Location;
+      userFound.avatar_url = uploadResult.Location;
 
-      const result = await userFound.save();
-      if (!result) {
-        res.status(400).json({ error: "User not updated" });
+      const updatedUser = await userFound.save();
+      if (!updatedUser) {
+        res.status(400).json({ error: "User Not Updated" });
       }
 
-      result.password = "";
+      updatedUser.password = "";
 
-      return res.status(200).json(result);
+      return res.status(200).json(updatedUser.dataValues);
     } catch (error) {
       return res.status(500).json(error);
     }
   }
 
   async login(req, res) {
+    const { body } = req;
+    const { email, password } = body;
+
     try {
       const schema = object({
         email: string().email().required(),
         password: string().required(),
       });
 
-      await schema.validate(req.body);
+      await schema.validate(body);
     } catch (error) {
       return res.status(400).json({ error: error?.message });
     }
 
     try {
-      const userFound = await Users.findOne({
-        where: { email: req.body.email },
-      });
+      const userFound = await Users.findOne({ where: { email: email } });
       if (!userFound) {
-        return res.status(401).json({ error: "User or password is invalid" });
+        return res.status(401).json({ error: "User or Password is Invalid" });
       }
 
-      const match = await bcrypt.compare(req.body.password, userFound.password);
+      const match = await bcrypt.compare(password, userFound.password);
       if (!match) {
-        return res.status(401).json({ error: "User or password is invalid" });
+        return res.status(401).json({ error: "User or Password is Invalid" });
       }
 
       const payload = { id: userFound.id };
